c4f64d371af894d5427ae585ff2bc3645e1de7d9
innocent Peter <peterotakhor@gmaile.com>
2023-09-07 15:17:47 -0700

    Q 101 i

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,8 @@
 #include <unistd.h>
-
+/**
+ * The main function is the entry point of the program.
+ * It prints the string "Programming is like building a multilingual puzzle".
+ */
 int main(void)
 {
   write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);

b8470b9770e8f53bca3ed905f8663d24b9011984
innocent Peter <peterotakhor@gmaile.com>
2023-09-07 14:33:29 -0700

    Q5 using betty coding style iii

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * This function prints the text "with proper grammar, but the outcome is a piece of art,"
+ * followed by a new line.
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
   printf("with proper grammar, but the outcome is a piece of art,\n");

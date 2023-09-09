27c85bfec45cdd8bde2b749a4198fea0a3d2f1f9
innocent Peter <peterotakhor@gmaile.com>
2023-09-09 08:01:46 -0700

    added only 4 putchar

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,28 +7,16 @@
  */
 int main(void)
 {
-  int digit1, digit2, digit3;
+  int digit;
 
-  for (digit1 = 0; digit1 <= 9; digit1++)
+  for (digit = 0; digit <= 9; digit++)
     {
-      for (digit2 = digit1 + 1; digit2 <= 9; digit2++)
-	{
-	  for (digit3 = digit2 + 1; digit3 <= 9; digit3++)
-	    {
-	      putchar(digit1 + '0');
-	      putchar(',');
-	      putchar(' ');
-	      putchar(digit2 + '0');
-	      putchar(',');
-	      putchar(' ');
-	      putchar(digit3 + '0');
+      putchar(digit + '0');
 
-	      if (digit1 != 7 || digit2 != 8 || digit3 != 9)
-		{
-		  putchar(',');
-		  putchar(' ');
-		}
-	    }
+      if (digit != 9)
+	{
+	  putchar(',');
+	  putchar(' ');
 	}
     }
 

db029585c26a65aea783ec4fa34d8df57deccc1e
innocent Peter <peterotakhor@gmaile.com>
2023-09-09 08:27:01 -0700

    fix for Q 0 i

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,7 +2,15 @@
 #include <time.h>
 #include <stdio.h>
 
-/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * This program assigns a random number to the variable n each time it is
+ * executed and prints whether the number stored in the variable n is positive
+ * or negative.
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
   int n;

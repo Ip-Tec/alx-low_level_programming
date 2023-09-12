599150970345bcd29288c84df1ac962933257766
innocent Peter <peterotakhor@gmaile.com>
2023-09-12 16:52:50 -0700

    Q 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,24 @@
-#include "main.h"
+/*
+ * File: 0-putchar.c
+ * Auth: Samuel Kioko
+ */
 
+#include "main.h"
 /**
- * main - check the code
+ * main - prints _putchar, followed by a new line.
  *
- * Return: Always 0.
+ * Return: Always 0
  */
 int main(void)
 {
-_putchar('_');
-_putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
-_putchar('r');
-_putchar('\n');
-return (0);
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
 }
---
+++ 0x02-functions_nested_loops/main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+print_alphabet();
+print_alphabet_x10();
+print_sign(98);
+print_last_digit(INT_MAX);
+jack_bauer();
+times_table();
+print_to_98(0);
+return (0);
+}
